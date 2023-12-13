c192764227ada4e73d12f80d06a2c662de8c9832
Erik Hoelzel <erik.hoelzel@posteo.de>
2023-12-13 06:31:04 +0100

    adds a new feature

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ public class Main {
         int count =0;
         while (true) {
             Thread.sleep(2*1000);
+            System.out.println("Jetzt gehts los");
             System.out.println(format("App running ... %d", count++));
         }
     }
